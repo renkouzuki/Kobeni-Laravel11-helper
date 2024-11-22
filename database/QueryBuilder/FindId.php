4d65c3d0468327ed2c6928934a124652b35122f2
Haichhan <ppfreea1@gmail.com>
2024-11-22 20:56:45 +0700

    find by id query update

--- database/QueryBuilder/FindId.php
+++ database/QueryBuilder/FindId.php
@@ -3,5 +3,38 @@
 namespace Database\QueryBuilder;
 
 class FindId {
+    public function findById($Data, $id, $relations = null, $select = null)
+    {
+        $query = $this->buildQuery($Data, $relations, $select);
 
+        return $query->find($id);
+    }
+
+    public function findByIds($Data, $ids, $relations = null, $select = null)
+    {
+        $query = $this->buildQuery($Data, $relations, $select);
+
+        return $query->whereIn('id', $ids)->get();
+    }
+
+    private function buildQuery($Data, $relations = null, $select = null)
+    {
+        $query = $Data::query();
+
+        if ($relations) {
+            foreach ($relations as $relation => $closure) {
+                if ($closure instanceof \Closure) {
+                    $query->with([$relation => $closure]);
+                } else {
+                    $query->with($relation);
+                }
+            }
+        }
+
+        if ($select) {
+            $query->select($select);
+        }
+
+        return $query;
+    }
 }
