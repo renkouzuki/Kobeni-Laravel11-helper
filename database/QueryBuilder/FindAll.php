c6070761862bced971eba6b2b2d37b1cd58369d9
Haichhan <ppfreea1@gmail.com>
2024-11-22 18:43:46 +0700

    findAll query builder

--- database/QueryBuilder/FindAll.php
+++ database/QueryBuilder/FindAll.php
@@ -3,5 +3,35 @@
 namespace Database\QueryBuilder;
 
 class FindAll {
+    public function allWithPagination($Data , $sort , $perPage = 10 , $relations = null , $select = null){
+        $query = $Data::query();
 
+        if ($relations) {
+            $query->with($relations);
+        }
+
+        if ($select) {
+            $query->select($select);
+        }
+    
+        if (!is_array($sort)) {
+            switch ($sort) {
+                case 'latest':
+                    $query->latest();
+                    break;
+                case 'oldest':
+                    $query->oldest();
+                    break;
+            }
+        } else {
+            $query->orderBy($sort[0], $sort[1]);
+        }
+    
+        return $query->paginate($perPage);
+    }
+
+    public function allWithLimit(){
+
+    }
 }
+
